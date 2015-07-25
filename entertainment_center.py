e8800e8d8eb69e0692a1d253bda503a384280060
Patricio Jeri <pjeri@hotmail.com>
2015-07-25 12:07:27 -0500

    Revised README, added 'year' to Movie class, movie cards now show in addition 'year' and plot.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,25 +1,32 @@
-import fresh_tomatoest
+import fresh_tomatoes
 import media
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life.",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+toy_story = media.Movie(
+    "Toy Story", "1995",
+    "A story of a boy and his toys that come to life.",
+    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-school_of_rock = media.Movie(   "School of Rock",
-                                "Overly enthusiastic guitarist Dewey Finn (Jack Black) gets thrown out of his bar band and finds himself in desperate need of work.",
-                                "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                                "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+school_of_rock = media.Movie("School of Rock", "2003",
+    "Overly enthusiastic guitarist Dewey Finn (Jack Black) gets thrown \
+    out of his bar band and finds himself in desperate need of work.",
+    "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg", # noqa
+    "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
 
-the_imitation_game = media.Movie(   "The Imitation Game",
-                                    "In 1939, newly created British intelligence agency MI6 recruits Cambridge mathematics alumnus Alan Turing (Benedict Cumberbatch) to crack Nazi codes, including Enigma",
-                                    "http://t0.gstatic.com/images?q=tbn:ANd9GcQQ5vi9xgRkP0nk5aRn8tcGEGRnOQyM-aAS1ldqfQSi_69V1yfU",
-                                    "https://www.youtube.com/watch?v=S5CjKEFb-sM")
+the_imitation_game = media.Movie(
+    "The Imitation Game", "2014",
+    "In 1939, newly created British intelligence agency MI6 recruits \
+    Cambridge mathematics alumnus Alan Turing (Benedict Cumberbatch) to \
+    crack Nazi codes, including Enigma",
+    "http://t0.gstatic.com/images?q=tbn:ANd9GcQQ5vi9xgRkP0nk5aRn8tcGEGRnOQyM-aAS1ldqfQSi_69V1yfU", # noqa
+    "https://www.youtube.com/watch?v=S5CjKEFb-sM")
 
-whiplash = media.Movie( "Whiplash", 
-                        "Andrew Neiman (Miles Teller) is an ambitious young jazz drummer, in pursuit of rising to the top of his elite music conservatory.",
-                        "http://t3.gstatic.com/images?q=tbn:ANd9GcS_IwW5-_mWA1PXiPG4qEhLC6Q3vntQd7Bzgs_YE7HHFifItn2T",
-                        "https://www.youtube.com/watch?v=7d_jQycdQGo")
+whiplash = media.Movie(
+    "Whiplash", "2014",
+    "Andrew Neiman (Miles Teller) is an ambitious young jazz drummer, \
+    in pursuit of rising to the top of his elite music conservatory.",
+    "http://t3.gstatic.com/images?q=tbn:ANd9GcS_IwW5-_mWA1PXiPG4qEhLC6Q3vntQd7Bzgs_YE7HHFifItn2T", # noqa
+    "https://www.youtube.com/watch?v=7d_jQycdQGo")
 
 movies = [toy_story, school_of_rock, the_imitation_game, whiplash]
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,11 +1,21 @@
 import webbrowser
 
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """
+    The "Movie" class represents a movie object with the following attributes:
+    Args:
+        param1(string): Title
+        param2(int): Release Year
+        param3(string): Storyline
+        param4(string): Poster Image URL
+        param5(string): Trailer Youtube URL
+    """
+    def __init__(self, movie_title, movie_release_year, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        self.release_year = movie_release_year
 
     def show_trailer(self):
     	"""
